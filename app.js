5246ec2ebe4ca8c1e7cacc6044cbec51a95c0df4
Hanan Kavitz <hananke0@gmail.com>
2019-04-21 10:26:04 +0300

    start script added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 8080;
+const port = process.env.PORT || 8080;
 const app = express();
 app.get('/', (req, res, next) => {
     res.send('Hanan is great');
