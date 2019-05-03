53db474c78b530fb81acdba38abcc26263a60e35
Hanan Kavitz <hananke0@gmail.com>
2019-05-03 14:10:06 +0300

    read images from bucket

--- src/serveImagesList.ts
+++ src/serveImagesList.ts
@@ -4,6 +4,9 @@ import { Response, Request, NextFunction } from "express";
 var router = express.Router();
 const fs = require('fs');
 const path = require('path');
+const {Storage} = require('@google-cloud/storage');
+
+const bucketName = 'myfirstnodejsapp-238305.appspot.com';
 
 router.get('/images',(req: Request, res: Response, next: NextFunction) => {
     const staticLocation = 'static/outputs';
@@ -22,5 +25,32 @@ router.get('/images',(req: Request, res: Response, next: NextFunction) => {
 
     })
     
+});
+var getPublicThumbnailUrlForItem = (fileName: string): string => {
+    return `https://storage.googleapis.com/${bucketName}/${fileName}`
+  }
+router.get('/imagesInBucket',async (req: Request, res: Response, next: NextFunction) => {
+    // Creates a client
+    const storage = new Storage({
+        projectId: 'myfirstnodejsapp-238305', 
+        // keyFilename: 'path-to-the-private-key', // Replace with the path to the downloaded private key
+      });
+
+    
+    const staticLocation = 'https://console.cloud.google.com/storage/browser/nodejs-demo-238305/';
+    // Lists files in the bucket
+    try {
+        let [files] = await storage.bucket(bucketName).getFiles();
+       
+        files = files.map((file: any): string => {
+            return  getPublicThumbnailUrlForItem(file.name);
+        } );
+        res.status(200).send(files);
+        
+    } catch (err){
+        console.error(err);
+    }
+        
 })
+
 module.exports = router;
